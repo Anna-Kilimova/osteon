6247a8d63b9e0db26f2a3ece98ba08b932584ef7
Cherous <r.kilimov@gmail.com>
2023-11-29 21:34:55 +0200

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header'
+import { About } from './pages/About'
+import { Home } from './pages/Home'
+import { Prices } from './pages/Prices'
 
 function App() {
-  return (
-    <div className="App">
+  return <div className="App">
+    <BrowserRouter>
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/about' element={ <About /> } />
+        <Route path='/prices' element={ <Prices /> } />
+      </Routes>
+    </BrowserRouter>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
 
-  return <>Header, i love you</>
+  return <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/prices">Users</Link>
+      </li>
+    </ul>
+  </nav>
 }
